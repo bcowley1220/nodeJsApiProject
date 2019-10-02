b5046b29333522bdd46f44aa57fc25fa4ea1f2d6
Brendan Cowley <b.cowley1220@gmail.com>
2019-10-02 19:23:18 -0400

    search get from DB working. Need to figure out conditional

--- routes/database.js
+++ routes/database.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const database = express.Router();
+// const database = express.Router();
 const pool = require("../connection");
-let bookList = require("../routes/route");
+let route = require("../routes/route");
 
 // function selectAllBooks(req, res) {
 //   pool.query("select * from nodeBook").then(result => {
@@ -10,35 +10,42 @@ let bookList = require("../routes/route");
 // }
 
 // database.get("/database", selectAllBooks);
-function selectAllBooks(req, res) {
-  pool.query("select * from nodebook").then(result => {
-    res.json(result.rows);
-    // console.log(res);
-  });
-}
+// function selectAllBooks(req, res) {
+//   pool.query("select * from nodebook").then(result => {
+//     res.json(result.rows);
+//     // console.log(res);
+//   });
+// }
 
-database.get("/books", function(req, res) {
-  selectAllBooks(req, res);
-});
+// database.get("/books", function(req, res) {
+//   selectAllBooks(req, res);
+// });
 
-database.post("/books", (req, res) => {
-  pool
-    .query(
-      "insert into nodebook (title, authors, description, catagories, publisher, publisheddate, previewlink, timeofsearch ) values ($1::text, $2::text, $3::text, $4::int, $5::text, $6::text, $7::text, $8::text)",
-      [
-        bookList[0].title,
-        req.body.authors,
-        req.body.description,
-        req.body.catagories,
-        req.body.publisher,
-        req.body.publisheddate,
-        req.body.previewlink,
-        req.body.timeofsearch
-      ]
-    )
-    .then(() => {
-      selectAllBooks(req, res);
-      // console.log(req, res);
-    });
-});
-module.exports = database;
+// database.post("/books", (req, res) => {
+//   for (let book of bookQueryList) {
+//     pool
+//       .query(
+//         "insert into nodebook (title, authors, description, catagories, publisher, publisheddate, previewlink, timeofsearch ) values ($1::text, $2::text, $3::text, $4::int, $5::text, $6::text, $7::text, $8::text)",
+//         [
+//           book.title,
+//           book.authors,
+//           book.description,
+//           book.catagories,
+//           book.publisher,
+//           book.publisheddate,
+//           book.previewlink,
+//           book.timeofsearch
+//         ]
+//       )
+//       .then(() => {
+//         selectAllBooks(req, res);
+//         // console.log(req, res);
+//       });
+//   }
+// });
+
+// console.log(route.bookQuery);
+module.exports = {
+  databaseRoute: database
+  // getCall: databaseGetCall()
+};
--- routes/route.js
+++ routes/route.js
@@ -1,76 +1,126 @@
+// var exports = (module.exports = {});
 let express = require("express");
 let router = express.Router();
+let database = express.Router();
+
 let https = require("https");
 let myKey = require("../api-key/api");
+const pool = require("../connection");
+// let database = require("../routes/database");
 
-let bookQuery = "One Second After";
+let bookQuery = "Patient";
 let body = "";
+let bookQueryList = [];
+let bodyItems;
+let length = 150;
 
-let url = `https://www.googleapis.com/books/v1/volumes?q=${bookQuery}:keyes&key=${myKey}`;
-https.get(url, res => {
-  res.setEncoding("utf8");
-  // let body = "";
-  res.on("data", data => {
-    body += data;
+let url = `https://www.googleapis.com/books/v1/volumes?q=${bookQuery}?orderBy=relevance:keyes&key=${myKey}`;
+
+async function gettingBookInfo() {
+  await database.get("/checkdatabase", function(req, res) {
+    checkDatabaseFirst(req, res);
   });
-  res.on("end", () => {
-    body = JSON.parse(body);
-    console.log(body);
-  });
-  return body;
-});
+  if (checkDatabaseFirst) {
+    await https.get(url, res => {
+      res.setEncoding("utf8");
+      // let body = "";
+      res.on("data", data => {
+        body += data;
+      });
+      res.on("end", () => {
+        body = JSON.parse(body);
+        // console.log(body);
+      });
+      return body;
+    });
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    await makingBookObject();
+  } else {
+    console.log("its there");
+  }
+}
 
-// async function gettingBookInfo() {
-//   await https
-//     .get(
-//       `https://www.googleapis.com/books/v1/volumes?q=${bookQuery}:keyes&key=${myKey}`,
-//       resp => {
-//         // let data = "";
-//         resp.on("data", chunk => {
-//           responseData += chunk;
-//         });
-//         resp.on("end", () => {
-//           console.log(responseData);
-//           // bookQueryResult = data;
-//           // console.log(bookQueryResult);
-//         });
-//         // return bookQueryResult;
-//       }
-//     )
-//     .on("error", err => {
-//       console.log("Error: " + err.message);
-//     });
-//   // await new Promise(resolve => setTimeout(resolve, 3000));
-//   // await makingBookObject();
-// }
+let makingBookObject = () => {
+  bodyItems = body.items;
+  // console.log(bodyItems);
+  for (let book of bodyItems) {
+    // console.log(book);
+    // console.log(book.volumeInfo.categories);
+    bookObject = {
+      title: book.volumeInfo.title,
+      authors: `${book.volumeInfo.authors}`,
+      description: `${stringLimit(book)}`,
+      catagories: `${book.volumeInfo.categories}`,
+      publisher: book.volumeInfo.publisher,
+      publishedDate: book.volumeInfo.publishedDate,
+      previewLink: book.volumeInfo.previewLink,
+      queryDate: `${new Date()}`
+    };
+    // console.log(bookObject);
+    bookQueryList.push(bookObject);
+  }
+  for (let i = 0; i < bookQueryList.length; i++) {
+    console.log(bookQueryList[i].description);
+  }
+  return bookQueryList;
+};
 
-// let makingBookObject = () => {
-//   // console.log(bookListItems);
-//   let entriesObject = Object.entries(bookQueryResult);
-//   console.log(entriesObject);
-//   // let holderObject;
-//   // for (let i = 0; i < bookQueryResult.items.length; i++) {
-//   //   // console.log(bookQueryResult.items[i].volumeInfo);
-//   //   holderObject = {
-//   //     title: bookQueryResult.items[i].volumeInfo.title,
-//   //     authors: bookQueryResult.items[i].volumeInfo.authors,
-//   //     description: bookQueryResult.items[i].volumeInfo.description,
-//   //     catagories: bookQueryResult.items[i].volumeInfo.catagories,
-//   //     publisher: bookQueryResult.items[i].volumeInfo.publisher,
-//   //     publishedDate: bookQueryResult.items[i].volumeInfo.publishedDate,
-//   //     previewLink: bookQueryResult.items[i].volumeInfo.previewLink,
-//   //     timeOfSearch: new Date()
-//   //   };
-//   //   console.log(holderObject);
-//   //   bookList.push(holderObject);
-//   // }
-//   // return bookList;
-// };
+let stringLimit = arr => {
+  if (!arr.volumeInfo.description) {
+    return "Description Unavailable";
+  } else {
+    return arr.volumeInfo.description.substring(0, length);
+  }
+};
 
-// gettingBookInfo();
-
+gettingBookInfo();
 router.get("/googleBook", (req, res) => {
-  res.send(body.items);
+  res.send(bookQueryList);
 });
 
-module.exports = router;
+// ! Database calls
+
+let checkDatabaseFirst = (req, res) => {
+  pool
+    .query(`select title from nodebook where title like '${bookQuery}%';`)
+    .then(result => {
+      res.json(result.rows);
+    });
+};
+
+let selectAllBooks = (req, res) => {
+  pool.query("select * from nodebook order by index").then(result => {
+    res.json(result.rows);
+  });
+};
+
+database.get("/books", function(req, res) {
+  selectAllBooks(req, res);
+});
+
+database.post("/books", (req, res) => {
+  for (let book of bookQueryList) {
+    pool
+      .query(
+        "insert into nodebook (title, authors, description, catagories, publisher, publisheddate, previewlink, timeofsearch) values ($1::text, $2::text, $3::text, $4::text, $5::text, $6::text, $7::text, $8::text)",
+        [
+          `${book.title}`,
+          `${book.authors}`,
+          `${book.description}`,
+          `${book.catagories}`,
+          `${book.publisher}`,
+          `${book.publishedDate}`,
+          `${book.previewLink}`,
+          `${book.queryDate}`
+        ]
+      )
+      .then(() => {
+        selectAllBooks(req, res);
+      });
+  }
+});
+
+module.exports = {
+  router: router,
+  database: database
+};
